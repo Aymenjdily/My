ba90a1ea95c44f6d2de34ea4c932f3314e5909a7
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-06-19 15:43:05 +0100

    new push3

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const ProjectCard = ({data : {slug, mainImage, projectName, url}, index}) => {
   return (
-    <Link href={`${url}`}>
+    <Link href="#">
       <div 
         className={`relative h-[60vh] w-full shadow-lg cursor-pointer`}
       >
