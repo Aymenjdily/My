91c3681c1a942c6f9425d8088aef76e3e7893dc0
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-06-28 16:02:34 +0100

    live previews

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,14 +5,21 @@ import Link from 'next/link'
 
 const ProjectCard = ({data : {slug, mainImage, projectName, url}, index}) => {
   return (
-    <Link href="#">
+    <Link href={`${url ? url : "#"}`}>
       <div 
         className={`relative h-[60vh] w-full shadow-lg cursor-pointer`}
       >
           <Image src={urlFor(mainImage).url()} fill alt="project-Image" className='object-cover' />
           <div className={`absolute w-full h-full bg-black/40`}>
-              <div className='flex items-end justify-start h-full'>
-                  <h1 className={`text-white text-2xl underline m-6 py-2 px-5 capitalize font-bold `}>
+              <div className='flex flex-col items-end justify-end h-full'>
+                  {
+                    url && (
+                      <p className='ml-6 cursor-pointer text-green-300 underline font-bold px-5'>
+                      Live Preview
+                      </p>
+                    )
+                  }
+                  <h1 className={`text-white text-2xl underline mb-3 py-2 px-5 capitalize font-bold `}>
                       {projectName}
                   </h1>
               </div>
