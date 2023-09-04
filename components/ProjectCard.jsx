233a4d7856ddbadc1f9fcd03dd8ab2f9f529fa0c
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-09-04 14:25:48 +0100

    some changes

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -7,10 +7,10 @@ const ProjectCard = ({data : {slug, mainImage, projectName, url}, index}) => {
   return (
     <Link href={`${url ? url : "#"}`}>
       <div
-        className={`relative h-[50vh] w-full duration-200 hover:opacity-80 shadow-lg cursor-pointer`}
+        className={`relative h-[50vh] w-full duration-200 hover:opacity-80 rounded-xl shadow-lg cursor-pointer`}
       >
           <Image src={urlFor(mainImage).url()} fill alt="project-Image" className='object-cover' />
-          <div className={`absolute w-full h-full bg-black/50`}>
+          <div className={`absolute rounded-xl w-full h-full bg-black/30`}>
               <div className='flex flex-col items-center justify-center h-full'>
                   <h1 className={`text-white text-center leading-[50px] text-4xl underline decoration-primary underline-offset-8 mb-3 py-2 px-5 capitalize font-bold `}>
                       {projectName}
